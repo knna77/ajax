812f2b3f4bcb25fe02370fcdccdcbe087ebb5baa
knna <knna77@gmail.com>
2021-01-13 22:25:00 +0100

    exer1 i exer2

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -21,6 +21,28 @@ function clearFields() {
     console.log(element);
   });
 }
+
+async function postUser(user) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'aplication/json;charset=UTF-8',
+    },
+    body: JSON.stringify(user),
+  };
+  fetch(POSTMAN_URL, options)
+    .then((res) => res.json())
+    .catch((error) => console.log('Error:', error))
+    .then((response) => {
+      console.log('user:' + user.first_name);
+      document.getElementById('userBD').innerHTML = user.first_name;
+      document.getElementById('statusBD').innerHTML = '200';
+      // span_user_name.innerHTML = user.first_name;
+      //span_status.innerHTML = '200';
+      console.log('guardado:', response);
+    });
+}
+
 function contruUrl(url, parameters) {
   let values = '?';
   for (i in parameters) {
@@ -54,14 +76,14 @@ function procesarFetch(numsecs, user) {
       .then((data) => {
         let response = JSON.parse(data);
         let usuari = response.data;
-        console.log('------' + data);
+        postUser(usuari);
         document.getElementById('id').innerHTML = usuari.id;
         document.getElementById('email').innerHTML = usuari.email;
         document.getElementById('name').innerHTML = usuari.first_name;
         //document.getElementById('status').innerHTML = response.status;
       })
       .catch((err) => {
-        document.getElementById('status').innerHTML = err.messag;
+        document.getElementById('status').innerHTML = err.message;
         console.error('ERROR: ', err.message);
       });
   }, numsecs * 1000);
